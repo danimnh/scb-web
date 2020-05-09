1d63c48b1620e84653dcf2fa2b70dfe645e5621b
NgenzSky <ardianarian18@yahoo.co.id>
2020-05-09 22:25:44 +0700

    initial

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,57 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+  <Navbar bg="white" expand="lg">
+    <Navbar.Brand href="#home" className="mx-5 px-5">
+      <img
+        src="https://www.cendekiabaznas.sch.id/wp-content/uploads/2017/12/Logo-SCB_Hitam.png"
+        alt=""
+        height="70px"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home" className="px-3">
+          Beranda
+        </Nav.Link>
+        <NavDropdown
+          title="Tentang Kami"
+          id="basic-nav-dropdown"
+          className="px-3"
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Informasi" id="basic-nav-dropdown" className="px-3">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#home" className="px-3">
+          Kegiatan
+        </Nav.Link>
+        <Nav.Link href="#home" className="px-3">
+          PPDB
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,193 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Banner from "../components/banner"
 import SEO from "../components/seo"
+import { Button } from "react-bootstrap"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Beranda" />
+    <Banner />
+    <div className="container text-center my-5 py-5">
+      <div className="lead">PROFIL</div>
+      <div className="display-3">Sekolah Cendekia BAZNAS</div>
+      <p className="m-5 m-5 ">
+        Sekolah Cendekia Baznas adalah sekolah inklusi bebas biaya dan berasrama
+        bagi dhuafa dan yatim untuk putra-putri Indonesia yang berada di bawah
+        pengelolaan Badan Amil Zakat Nasional (BAZNAS).
+      </p>
+      <span className="px-3">
+        <Button className="px-4 py-3">SCB dalam Angka</Button>
+      </span>
+      <span className="px-3">
+        <Button className="px-4 py-3">Agenda</Button>
+      </span>
+      <span className="px-3">
+        <Button className="px-4 py-3">Berita terkini</Button>
+      </span>
+      <span className="px-3">
+        <Button className="px-4 py-3">SCB dalam 24Jam</Button>
+      </span>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="container text-center my-5 py-5">
+      <div className="lead">JELAJAHI</div>
+      <div className="display-3">SCB dalam Angka</div>
+      <div className="row py-4">
+        <div className="col">
+          <div className="card text-center">
+            <div className="card-body">
+              <div className="display-4">44</div>
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card text-center">
+            <div className="card-body">
+              <div className="display-4">44</div>
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card text-center">
+            <div className="card-body">
+              <div className="display-4">44</div>
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card text-center">
+            <div className="card-body">
+              <div className="display-4">44</div>
+              <p>Lorem ipsum dolor sit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container text-center my-5 py-5">
+      <div className="display-3">Penerimaan Siswa Baru</div>
+      <div className="lead">HITUNG MUNDUR</div>
+      <div className="card-group py-5">
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">
+              <span className="display-4">00</span>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">HARI</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">
+              <span className="display-4">00</span>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">JAM</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">
+              <span className="display-4">00</span>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">MENIT</small>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">
+              <span className="display-4">00</span>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">DETIK</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container my-5 py-5">
+      <div className="display-4">Agenda</div>
+      <div className="row pt-5 pb-2">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5">
+                  <div className="display-4">20</div>
+                  <div className="h3">Nov</div>
+                </div>
+                <div className="col">
+                  <div>Gema Akbar</div>
+                  <div>Lorem, ipsum.</div>
+                  <div>Lorem, ipsum.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5">
+                  <div className="display-4">20</div>
+                  <div className="h3">Nov</div>
+                </div>
+                <div className="col">
+                  <div>Gema Akbar</div>
+                  <div>Lorem, ipsum.</div>
+                  <div>Lorem, ipsum.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5">
+                  <div className="display-4">20</div>
+                  <div className="h3">Nov</div>
+                </div>
+                <div className="col">
+                  <div>Gema Akbar</div>
+                  <div>Lorem, ipsum.</div>
+                  <div>Lorem, ipsum.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-5">
+                  <div className="display-4">20</div>
+                  <div className="h3">Nov</div>
+                </div>
+                <div className="col">
+                  <div>Gema Akbar</div>
+                  <div>Lorem, ipsum.</div>
+                  <div>Lorem, ipsum.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="float-right">Lihat lebih lengkap</div>
+    </div>
   </Layout>
 )
 
